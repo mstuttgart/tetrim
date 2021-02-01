d1b53971e3daf1994d7ea615513daaac82bad66f
Michell Stuttgart <michellstut@gmail.com>
2021-02-01 06:58:17 -0300

    Replace tab by space

--- tetraminos/Board.gd
+++ tetraminos/Board.gd
@@ -4,27 +4,27 @@ const TILE_SIZE = 32
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+    pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # warning-ignore:unused_argument
 func _process(delta):
 
-	var velocity = Vector2()
+    var velocity = Vector2()
 
-	if Input.is_action_just_pressed("ui_up"):
-		$S.rotate_block()
+    if Input.is_action_just_pressed("ui_up"):
+        $Player.rotate_block()
 
-	elif Input.is_action_just_pressed("ui_left"):
-		velocity.x -= 1
+    elif Input.is_action_just_pressed("ui_left"):
+        velocity.x -= 1
 
-	elif Input.is_action_just_pressed("ui_right"):
-		velocity.x += 1
+    elif Input.is_action_just_pressed("ui_right"):
+        velocity.x += 1
 
-	elif Input.is_action_just_pressed("ui_down"):
-		velocity.y += 1
+    elif Input.is_action_just_pressed("ui_down"):
+        velocity.y += 1
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * TILE_SIZE
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * TILE_SIZE
 
-	$S.move_and_collide(velocity)
+    $Player.move_and_collide(velocity)
