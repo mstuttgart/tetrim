e4dee8b56966f5a68d0d5fd99c609262dc032e88
Michell Stuttgart <michellstut@gmail.com>
2021-02-03 07:21:54 -0300

    Add ANGLE const in S.gd script

--- tetraminos/blocks/S.gd
+++ tetraminos/blocks/S.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
+const ANGLE = PI/2
+
 func rotate_block():
 
     if not _check_collision_on_rotate():
-        rotation += PI/2
+        rotation += ANGLE
 
 func _check_collision_on_rotate():
-    var transform2d = Transform2D(rotation + PI/2, position)
+    var transform2d = Transform2D(rotation + ANGLE, position)
     return $RayCast2D_1.is_colliding() or $RayCast2D_2.is_colliding() or test_move(transform2d, Vector2(0, 0), false)
