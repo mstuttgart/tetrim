36e542209f4de116aec65b3b15b134443b3f5fba
Michell Stuttgart <michellstut@gmail.com>
2021-03-17 18:13:44 -0300

    Change QUIT button on board to redirect to 'Title Screen'

--- tetraminos/GUI.gd
+++ tetraminos/GUI.gd
@@ -35,4 +35,5 @@ func _on_ButtonPauseResume_pressed():
 
 
 func _on_ButtonQuit_pressed():
-    get_parent().get_tree().quit()
+    get_tree().change_scene("res://TitleScreen.tscn")
+#    get_parent().get_tree().free()
