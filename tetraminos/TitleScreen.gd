6573f41082bdddc1cac745a2834dc0eefabfece7
Michell Stuttgart <michellstut@gmail.com>
2021-03-16 21:05:20 -0300

    Add TitleScreen scene

---
+++ tetraminos/TitleScreen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+    $FadeBackground/AnimationPlayer.play("FadeIn")
+
+func _on_Play_pressed():
+    $FadeBackground.visible = true
+    $FadeBackground/AnimationPlayer.play("FadeOut")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+
+    if anim_name == 'FadeOut':
+        get_tree().change_scene("res://Game.tscn")
+    else:
+        $FadeBackground.visible = false
